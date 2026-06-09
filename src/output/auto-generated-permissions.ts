332c0f0cbb3169ccd640bf84425d15aa54d08dcf
edilson258 <dev.258.edilson@gmail.com>
2026-06-09 14:13:21 +0200

    include output folder

---
+++ src/output/auto-generated-permissions.ts
@@ -0,0 +1,28 @@
+// AUTO-GENERATED FILE. DO NOT EDIT DIRECTLY.
+
+export type Permission =
+  | "promotion.farmers.manage.create"
+  | "promotion.farmers.manage.delete"
+  | "promotion.farmers.manage.export"
+  | "promotion.farmers.manage.module"
+  | "promotion.farmers.manage.print"
+  | "promotion.farmers.manage.read"
+  | "promotion.farmers.manage.update"
+  | "promotion.farmers.module"
+  | "promotion.farmers.transfer.module"
+  | "promotion.farmers.transfer.transfer_farmer"
+  | "promotion.module";
+
+export const PERMISSIONS = [
+  "promotion.farmers.manage.create",
+  "promotion.farmers.manage.delete",
+  "promotion.farmers.manage.export",
+  "promotion.farmers.manage.module",
+  "promotion.farmers.manage.print",
+  "promotion.farmers.manage.read",
+  "promotion.farmers.manage.update",
+  "promotion.farmers.module",
+  "promotion.farmers.transfer.module",
+  "promotion.farmers.transfer.transfer_farmer",
+  "promotion.module",
+] as const;
