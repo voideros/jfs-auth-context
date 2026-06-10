8c3e26c8bcf0c230162e8b19b1dc5103f1320ace
edilson258 <dev.258.edilson@gmail.com>
2026-06-10 09:39:51 +0200

    chore: deploy to voideros public repo

--- src/output/auto-generated-permissions.ts
+++ src/output/auto-generated-permissions.ts
@@ -1,28 +1,36 @@
 // AUTO-GENERATED FILE. DO NOT EDIT DIRECTLY.
 
 export type Permission =
+  | "helpdesk.module"
   | "promotion.farmers.manage.create"
   | "promotion.farmers.manage.delete"
   | "promotion.farmers.manage.export"
   | "promotion.farmers.manage.module"
-  | "promotion.farmers.manage.print"
   | "promotion.farmers.manage.read"
   | "promotion.farmers.manage.update"
   | "promotion.farmers.module"
   | "promotion.farmers.transfer.module"
   | "promotion.farmers.transfer.transfer_farmer"
-  | "promotion.module";
+  | "promotion.module"
+  | "promotion.questionnaire.analysis.export"
+  | "promotion.questionnaire.analysis.module"
+  | "promotion.questionnaire.analysis.read"
+  | "promotion.questionnaire.module";
 
 export const PERMISSIONS = [
+  "helpdesk.module",
   "promotion.farmers.manage.create",
   "promotion.farmers.manage.delete",
   "promotion.farmers.manage.export",
   "promotion.farmers.manage.module",
-  "promotion.farmers.manage.print",
   "promotion.farmers.manage.read",
   "promotion.farmers.manage.update",
   "promotion.farmers.module",
   "promotion.farmers.transfer.module",
   "promotion.farmers.transfer.transfer_farmer",
   "promotion.module",
+  "promotion.questionnaire.analysis.export",
+  "promotion.questionnaire.analysis.module",
+  "promotion.questionnaire.analysis.read",
+  "promotion.questionnaire.module",
 ] as const;
